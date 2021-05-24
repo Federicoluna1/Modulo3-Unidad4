14e910c3e20f01864539dcf8e5900a10ac858088
Fede <84208153+Federicoluna1@users.noreply.github.com>
2021-05-24 19:20:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const mysql = require('mysql');
+const util = require ('util')
+const app = express();
+
+
+app.use(express.json());
+
+const puerto = 3000
+
+//conexion con Base de datos
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'',
+    database: 'suscriptores'
+});
+
+const qy = util.promisify (conexion. query.bind(conexion));
+
+conexion.connect((error)=>{
+    if(error) {
+        throw error;
+    }
+    
+    console.log ('Conexión con base de datos mysql establecida');
+    });;
+
+
+//GET
+app.get ('/misDatos', async (req, res)=>{
+    try {
+        const query = 'SELECT * FROM suscriptores';
+        
+        const respuesta = await qy (query);
+
+        if (respuesta.lenght == 0) {
+            throw new Error ('No hay suscriptor con esos datos');
+
+        res.send ({'respuesta': respuesta});
+    }
+    }
+    catch(e) {
+        console.error(e.message);
+        res.status(413).send({"Error": e.message});
+    }
+});
+
+//POST
+app.post('/suscripcion', async (req, res ) =>{
+    try {
+
+        let nombre = req.body.nombre.toUpperCase();
+        let apellido = req.body.apellido.toUpperCase();
+        let dni = req.body.dni.toUpperCase();
+        let email = req.body.email.toUpperCase();
+       
+        function validateEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+        if (!nombre || !apellido || !dni || !email){
+        res.status(413).send('Faltan datos');
+    }
+
+        if (validateEmail(email) == false){
+        res.status(413).send('Email invalido');
+    }
+
+        let query = 'SELECT id FROM suscriptores WHERE email = ?'
+        let respuesta = await qy(query, [email]);
+
+        if (respuesta.length > 0) {
+            throw new Error ('Ese email ya esta registrado');
+    }
+    
+        conexion.query("INSERT INTO `suscriptores`(`nombre`, `apellido`, `dni`, `email`)" +
+        "VALUES ('"+nombre+"','"+apellido+"','"+dni+"','"+email+"')");
+        var html ="<html><head><title>SeHaSuscripto</title></head><body><h1>Se ha suscripto correctamente a nuestro Newsletter</h1>" +
+        "<a href='/'>Volver al Blog</a></body></html>";
+        res.send(html);
+    }
+
+    catch(e) {
+        console.error(e.message);
+        res.status(413).send({"Error": e.message});
+    }
+});
+
+
+app.listen (3000, ()=>{
+    console.log ('Servidor  escuchando en el puerto 3000')
+});
+    
